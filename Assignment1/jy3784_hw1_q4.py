4f6f27f046c79363e2181e2b83faa7ce64e1efee
Jack Yang <jack73117311@gmail.com>
2025-04-24 01:22:42 -0400

    Delete Assignment1 directory

--- Assignment1/jy3784_hw1_q4.py
+++
@@ -1,9 +0,0 @@
-#a
-num1 = [10 ** i for i in range(6)]
-
-#b
-num2 = [i**2 + i for i in range(10)]
-
-#c
-num3 = [chr(97 + i) for i in range(26)]
-
--- Assignment1/jy3784_hw1_q5.py
+++
@@ -1,9 +0,0 @@
-def fibs(n):
-    fib = []
-    for i in range(n):
-        if i == 1 or i == 0:
-            fib.append(1)
-            yield 1
-        else:
-            fib.append(fib[i-1] + fib[i-2])
-            yield fib[i]
